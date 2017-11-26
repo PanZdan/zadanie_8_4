7b77871e408afe7791e5232ee2e839ed75974701
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2017-11-26 15:15:51 +0100

    Add comments for scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// scripts.js
+
+// komentarz liniowy
+
+/*
+komentarz bolokowy
+var name = prompt('Enter your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
+*/
+
+var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+console.log('Hello, ' + name);
